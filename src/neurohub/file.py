bf1c2db185801715a0848e2b17e0a29ec1ba0f98
Kamatayon <wowegable@gmail.com>
2025-09-20 21:45:50 +0800

    feat: add audio_channels and call_params arguments to post file

--- src/neurohub/file.py
+++ src/neurohub/file.py
@@ -4,33 +4,45 @@ from uuid import UUID
 from typing import IO
 
 
-CallType = Literal['Outgoing', 'Incoming', 'IncomingRedirection', 'Callback']
+CallType = Literal["Outgoing", "Incoming", "IncomingRedirection", "Callback"]
 
 
-class Files():
+class Files:
     def __init__(self, base: BaseClient):
         self._base = base
-    def post(self, manager_uuid: UUID | str, file_content: bytes | IO, checklist_uuid: UUID | str, call_id: str, call_type: Optional[CallType] = None, client_uuid: Optional[UUID | str] = None):
+
+    def post(
+        self,
+        manager_uuid: UUID | str,
+        file_content: bytes | IO,
+        checklist_uuid: UUID | str,
+        call_id: str,
+        call_type: Optional[CallType] = None,
+        client_uuid: Optional[UUID | str] = None,
+        audio_channels: Optional[dict] = None,
+        call_params: Optional[dict] = None,
+    ):
         if not self._base.s3_client:
-            raise RuntimeError("S3 client is not configured. Please provide AWS credentials when initializing the client.")
+            raise RuntimeError(
+                "S3 client is not configured. Please provide AWS credentials when initializing the client."
+            )
         client_uuid = self._base._handle_client_uuid(client_uuid)
 
         file_name = f"{call_id}.mp3"
         s3_key = f"input/{file_name}"
 
         self._base.s3_client.put_object(
-            Bucket=self._base.s3_bucket,
-            Key=s3_key,
-            Body=file_content
+            Bucket=self._base.s3_bucket, Key=s3_key, Body=file_content
         )
 
-        # TODO: add audio_channels and file_params to args
         body = {
-            'client_uuid': client_uuid,
-            'manager_uuid': manager_uuid,
-            'file_name': file_name,
-            'checklist_uuid': str(checklist_uuid),
-            'call_type': call_type
+            "client_uuid": client_uuid,
+            "manager_uuid": manager_uuid,
+            "file_name": file_name,
+            "checklist_uuid": str(checklist_uuid),
+            "call_type": call_type,
+            "audio_channels": audio_channels,
+            "call_params": call_params,
         }
-        resp = self._base.make_request('file', 'POST', body=body)
-        return UUID(resp['file_uuid'])
+        resp = self._base.make_request("file", "POST", body=body)
+        return UUID(resp["file_uuid"])
